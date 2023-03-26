7268e334adc9bb7ab6da7b074cd8343f390c738e
eddyzags <zagabe.ed@gmail.com>
2023-03-26 15:45:19 +0200

    scripts: support cert config for every kube instancs and rely on output folder for generated files

--- scripts/certificates_gen.sh
+++ scripts/certificates_gen.sh
@@ -1,26 +1,91 @@
 #!/bin/sh
 # Example:
-# ./cfssl_gen.sh worker-0:192.169.1,worker-1:192.169.2,worker-2:192.169.3
+# ./certificates_gen.sh worker-0:192.169.1.0,worker-1:192.169.1.1,worker-2:192.169.1.2
+
+OUTPUT_FOLDER=./output/certs/config
+
+
+if [ ! -d $OUTPUT_FOLDER ]
+then
+    echo "Creating $OUTPUT_FOLDER..."
+    mkdir -p $OUTPUT_FOLDER
+fi
 
 # Generate certificate authority certificate
 echo "Generating certificate authority certificate..."
+
+cat > output/certs/config/ca_csr.json <<EOF
+{
+    "CN": "Kubernetes",
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+        {
+            "C": "FR",
+            "L": "Lille",
+            "O": "Kubernetes",
+            "OU": "CA",
+            "ST": "Nord"
+        }
+    ]
+}
+EOF
+
+cat > output/certs/config/ca_config.json <<EOF
+{
+  "signing": {
+    "default": {
+      "expiry": "8760h"
+    },
+    "profiles": {
+      "kubernetes": {
+        "usages": ["signing", "key encipherment", "server auth", "client auth"],
+        "expiry": "8760h"
+      }
+    }
+  }
+}
+EOF
+
 cfssl gencert \
-      -initca ca_csr.json | cfssljson -bare ca
+      -initca output/certs/config/ca_csr.json | output/cfssl/bin/cfssljson -bare -output output/certs ca
 echo "Certificate authority certificate generated.\n\n"
 
 # Generate admin client certificate
 echo "Generating admin client certificate..."
+
+cat > output/certs/config/admin_csr.json <<EOF
+{
+    "CN": "admin",
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+        {
+            "C": "France",
+            "L": "Lille",
+            "O": "system:masters",
+            "OU": "CA",
+            "ST": "Nord"
+        }
+    ]
+}
+EOF
+
 cfssl gencert \
-      -ca=ca.pem \
-      -ca-key=ca-key.pem \
-      -config=ca_config.json \
+      -ca=output/certs/ca.pem \
+      -ca-key=output/certs/ca-key.pem \
+      -config=output/certs/config/ca_config.json \
       -profile=kubernetes \
-      admin_csr.json | cfssljson -bare admin
+      output/certs/config/admin_csr.json | output/cfssl/bin/cfssljson -bare -output output/certs admin
 echo "Admin client certificate genreated.\n\n"
 
 # Generate kubelet client certificates
 c=0
-limit=`echo $1 | sed 's/[^,]//g' | awk '{ print length + 1 }'`
+limit=`echo $1 | sed 's/[^,]//g' | awk '{ print length + 1 }'`;
 
 while [ $c -lt $limit ]
 do
@@ -29,13 +94,13 @@ do
 
     echo "Generating $workerName certificate..."
 
-    cat > "$workerName"-csr.json <<EOF
+    cat > output/certs/config/"$workerName"_csr.json <<EOF
 {
   "CN": "system:node:$workerName",
   "key": {
     "algo": "rsa",
     "size": 2048
-  },
+ },
   "names": [
     {
       "C": "France",
@@ -48,12 +113,12 @@ do
 }
 EOF
     cfssl gencert \
-          -ca=ca.pem \
-          -ca-key=ca-key.pem \
-          -config=ca-config.json \
+          -ca=output/certs/ca.pem \
+          -ca-key=output/certs/ca-key.pem \
+          -config=output/certs/config/ca_config.json \
           -hostname=$ip \
           -profile=kubernetes \
-          "$workerName"-csr.json | cfssljson -bare $workerName
+          output/certs/config/"$workerName"_csr.json | output/cfssl/bin/cfssljson -bare -output output/certs $workerName
 
     echo "$workerName certificate done\n\n"
     c=$((c + 1))
@@ -61,42 +126,122 @@ done
 
 # Generate kube-controller-manager certificate
 echo "Generating kube-controller-manager certificate..."
+
+cat > output/certs/config/kube_controller_manager_csr.json <<EOF
+{
+  "CN": "system:kube-controller-manager",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "France",
+      "L": "Lille",
+      "O": "system:kube-controller-manager",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Nord"
+    }
+  ]
+}
+EOF
+
 cfssl gencert \
-      -ca=ca.pem \
-      -ca=ca-key.pem \
-      -config=ca_config.json \
+      -ca=output/certs/ca.pem \
+      -ca-key=output/certs/ca-key.pem \
+      -config=output/certs/config/ca_config.json \
       -profile=kubernetes \
-      kube_controller_manager_csr.json | cfssljson -bare kube-controller-manager
+      output/certs/config/kube_controller_manager_csr.json | output/cfssl/bin/cfssljson -bare -output output/certs kube-controller-manager
 echo "Kube-controller-manager certificate generated.\n\n"
 
 
 # Generate kube-proxy certificate
 echo "Generating kube-proxy certificate..."
+
+cat > output/certs/config/kube_proxy_csr.json <<EOF
+{
+  "CN": "system:kube-proxy",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "France",
+      "L": "Lille",
+      "O": "system:node-proxier",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Nord"
+    }
+  ]
+}
+EOF
+
 cfssl gencert \
-      -ca=ca.pem \
-      -ca-key=ca-key.pem \
-      -config=ca_config.json \
+      -ca=output/certs/ca.pem \
+      -ca-key=output/certs/ca-key.pem \
+      -config=output/certs/config/ca_config.json \
       -profile=kubernetes \
-      kube_proxy_csr.json | cfssljson -bare kube-proxy
+      output/certs/config/kube_proxy_csr.json | output/cfssl/bin/cfssljson -bare -output output/certs kube-proxy
 echo "kube-proxy certificate generated.\n\n"
 
 # Generate kube-apiserver certificate
 echo "Generating kube-apiserver certificate..."
+
+cat > output/certs/config/kubernetes_csr.json <<EOF
+{
+    "CN": "kubernetes",
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+        {
+        "C": "FR",
+        "L": "France",
+        "O": "Kubernetes",
+        "OU": "Kubernetes The Hard Way",
+        "ST": "Nord"
+        }
+    ]
+}
+EOF
+
 cfssl gencert \
-      -ca=ca.pem \
-      -ca-key=ca-key.pem \
-      -config=ca_config.json \
+      -ca=output/certs/ca.pem \
+      -ca-key=output/certs/ca-key.pem \
+      -config=output/certs/config/ca_config.json \
       -hostname=192.168.1.50,127.0.0.1,kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local \
       -profile=kubernetes \
-      kubernetes_csr.json | cfssljson -bare kubernetes
-echo "kube-apiserver certificate generated.\n\n."
+      output/certs/config/kubernetes_csr.json | output/cfssl/bin/cfssljson -bare -output output/certs kubernetes
+echo "kube-apiserver certificate generated.\n\n"
 
 # Generate service account certificate
 echo "Generating service account certificate..."
+
+cat > output/certs/config/service_account_csr.json <<EOF
+{
+  "CN": "service-accounts",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "France",
+      "L": "Lille",
+      "O": "Kubernetes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Nord"
+    }
+  ]
+}
+EOF
+
 cfssl gencert \
-      -ca=ca.pem \
-      -ca-key=ca-key.pem \
-      -config=ca-config.json \
+      -ca=output/certs/ca.pem \
+      -ca-key=output/certs/ca-key.pem \
+      -config=output/certs/config/ca_config.json \
       -profile=kubernetes \
-      service_account_csr.json | cfssljson -bare service-account
+      output/certs/config/service_account_csr.json | output/cfssl/bin/cfssljson -bare -output output/certs service-account
 echo "service account certificate generated."
